75b14352e81be3692ac0603b21ca508b32c44f0f
ZhcnPanda <2623493447@qq.com>
2022-07-28 18:17:22 +0800

    cezanne: Add boot animation pixels

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
