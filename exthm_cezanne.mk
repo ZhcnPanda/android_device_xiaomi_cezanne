1404dcadc845cccfa2d7948b61eaabf8dfa0b236
ZhcnPanda <2623493447@qq.com>
2022-07-28 20:49:09 +0800

    cezanne: Called missing device.mk
    Co-Authored-By: HuaLiMao-AQ <86974632+HuaLimao-AQ@users.noreply.github.com>

--- exthm_cezanne.mk
+++ exthm_cezanne.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common ExThmUI stuff.
 $(call inherit-product, vendor/exthm/config/common.mk)
 
+# Inherit from cezanne device
+$(call inherit-product, device/xiaomi/cezanne/device.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := exthm_cezanne
 PRODUCT_DEVICE := cezanne
